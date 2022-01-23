3ebb39791729dc1b766b81e3ae905e8de4a0f3f8
K32ar <43698793+Cezar66614@users.noreply.github.com>
2022-01-23 17:47:04 +0200

    Update aes.cpp

--- aes.cpp
+++ aes.cpp
@@ -12,14 +12,18 @@ std::ifstream fin;
 std::string encrypt(std::string plain) {
   Botan::Processor_RNG rng;
   std::unique_ptr<Botan::Cipher_Mode> enc = Botan::Cipher_Mode::create("AES-256/SIV", Botan::ENCRYPTION); // Define encryption alg
-  if(!enc)
+  std::unique_ptr<Botan::MessageAuthenticationCode> mac(Botan::MessageAuthenticationCode::create("CMAC(AES-256)")); // define the MAC to use
+  if(!enc || !mac)
     return "error";
 
-  std::string x; // read key
+  std::string x, xs; // read key
   fin.open("key.txt"); fin >> x; fin.close();
-  const std::vector<uint8_t> key = Botan::hex_decode(x); x = " ";
+  for (int i = 0; i < 128; i += 2) xs += x[i];
+  const std::vector<uint8_t> aesKey = Botan::hex_decode(x); x = " ";
+  const std::vector<uint8_t> macKey = Botan::hex_decode(xs); xs = " ";
 
-  enc->set_key(key);
+  enc->set_key(aesKey);
+  mac->set_key(macKey);
 
   Botan::secure_vector<uint8_t> iv = rng.random_vec(enc->default_nonce_length()); // generate iv
 
@@ -28,82 +32,49 @@ std::string encrypt(std::string plain) {
   enc->start(iv);
   enc->finish(pt);
 
-  return Botan::hex_encode(iv) + Botan::hex_encode(pt); // return the cipher + the iv
+  // Create tag
+  std::vector<uint8_t> package = Botan::hex_decode(Botan::hex_encode(iv) + Botan::hex_encode(pt));
+  mac->update(package);
+
+  Botan::secure_vector<uint8_t> tag = mac->final();
+
+  return Botan::hex_encode(iv) + Botan::hex_encode(tag) + Botan::hex_encode(pt); // return iv + macTag + actual cipher
 }
 
 std::string decrypt(std::string cipher) {
   Botan::Processor_RNG rng;
   std::unique_ptr<Botan::Cipher_Mode> dec = Botan::Cipher_Mode::create("AES-256/SIV", Botan::DECRYPTION); // Define decryption alg (same as the encryption)
-  if(!dec)
+  std::unique_ptr<Botan::MessageAuthenticationCode> mac(Botan::MessageAuthenticationCode::create("CMAC(AES-256)")); // define the MAC to use
+  if(!dec || !mac)
     return "error";
 
-  std::string x; // read key
+  std::string x, xs; // read key
   fin.open("key.txt"); fin >> x; fin.close();
-  const std::vector<uint8_t> key = Botan::hex_decode(x); x = " ";
+  for (int i = 0; i < 128; i += 2) xs += x[i];
+  const std::vector<uint8_t> aesKey = Botan::hex_decode(x); x = " ";
+  const std::vector<uint8_t> macKey = Botan::hex_decode(xs); xs = " ";
 
-  dec->set_key(key);
+  dec->set_key(aesKey);
+  mac->set_key(macKey);
 
   for (int i = 0; i < 24; i++) x += cipher[i]; // get the iv from package
   std::vector<uint8_t> iv(Botan::hex_decode(x)); x = " ";
 
-  int i = 24;
+  for (int i = 24; i < 56; i++) x += cipher[i]; // get the tag from package
+  Botan::secure_vector<uint8_t> tag = Botan::hex_decode_locked(x); x = " ";
+
+  int i = 56;
   while (cipher[i]) { x += cipher[i]; i++; } // get the cipher from package
-  Botan::secure_vector<uint8_t> et(Botan::hex_decode_locked(x)); x = " ";
+  Botan::secure_vector<uint8_t> et(Botan::hex_decode_locked(x));
 
   dec->start(iv);
   dec->finish(et);
   
-  return std::string(et.data(), et.data()+et.size()); // return the decrypted text
-}
+  x = Botan::hex_encode(iv) + x;
+  Botan::secure_vector<uint8_t> cipherNoTag(Botan::hex_decode_locked(x)); x = " ";
+  mac->update(cipherNoTag);
 
-std::string mac(std::string package, bool u = 0) {
-  std::unique_ptr<Botan::MessageAuthenticationCode> mac(Botan::MessageAuthenticationCode::create("CMAC(AES-256)")); // define the MAC to use
-  if(!mac)
-    return "error";
-  
-  fin.open("key.txt"); // read MAC key
-  std::string key_s, x;
-  fin >> x; fin.close();
-  for (int i = 0; i < 128; i += 2) key_s += x[i]; x = " ";
-
-  const std::vector<uint8_t> key = Botan::hex_decode(key_s); key_s = " ";
-
-  mac->set_key(key);
-
-  if (!u) { // Add MAC to cipher
-    std::vector<uint8_t> data = Botan::hex_decode(package);
-    mac->update(data);
-
-    Botan::secure_vector<uint8_t> tag = mac->final();
-    
-    std::string iv;
-    for (int i = 0; i < 24; i++) iv += package[i]; // get the iv from package
-
-    std::string cipher;
-    int i = 24;
-    while (package[i]) { cipher += package[i]; i++; } // get the cipher from package
-
-    return iv + Botan::hex_encode(tag) + cipher;
-
-  } else { // Verify MAC of cipher
-    std::string macTag;
-    for (int i = 24; i < 56; i++) macTag += package[i]; // get the tag from package
-    Botan::secure_vector<uint8_t> tag = Botan::hex_decode_locked(macTag); macTag = " ";
-
-    std::string packageWithoutMAC;
-    for (int i = 0; i < 24; i++) packageWithoutMAC += package[i]; // get the iv from package
-
-    int i = 56;
-    while (package[i]) { packageWithoutMAC += package[i]; i++; } // get the cipher from package
-
-    std::vector<uint8_t> data = Botan::hex_decode(packageWithoutMAC);
-    mac->update(data);
-
-    return (mac->verify_mac(tag) ? "success\n" : "failure\n");
-
-  }
-
-  return "error";
+  return std::string(et.data(), et.data()+et.size()) + '\n' +(mac->verify_mac(tag) ? "success" : "failure"); // return the decrypted text
 }
 
 int main() {
@@ -122,11 +93,5 @@ int main() {
     std::cout << "An error in creating the algorithm has appeared\n";
     return 0;
   }
-
-  std::string encryptedWithMacTag = mac(encrypted);
-
-  std::cout << encryptedWithMacTag << '\n';
-
-  std::cout << "Verification: " << mac(encryptedWithMacTag, 1);
   return 0;
 }
